f4324c65164d9375d93959476e4948ac6ed1acc7
Diego <diego2.gd@gmail.com>
2017-06-07 23:15:42 +0200

    Practica 5.3

--- controllers/tip_controller.js
+++ controllers/tip_controller.js
@@ -5,7 +5,11 @@ var Sequelize = require('sequelize');
 // Autoload la pista asociado a :tipId
 exports.load = function (req, res, next, tipId) {
 
-    models.Tip.findById(tipId)
+    models.Tip.findById(tipId, {
+        include: [
+            {model: models.User, as: 'Author'}
+        ]
+    })
     .then(function (tip) {
         if (tip) {
             req.tip = tip;
@@ -17,6 +21,20 @@ exports.load = function (req, res, next, tipId) {
     .catch(function (error) {
         next(error);
     });
+};
+
+// MW que permite acciones solamente si al usuario logeado es admin o es el autor del tip.
+exports.adminOrAuthorRequired = function(req, res, next){
+
+    var isAdmin  = req.session.user.isAdmin;
+    var isAuthor = req.tip.AuthorId === req.session.user.id;
+
+    if (isAdmin || isAuthor) {
+        next();
+    } else {
+        console.log('Operación prohibida: El usuario logeado no es el autor del tip, ni un administrador.');
+        res.send(403);
+    }
 };
 
 
@@ -37,10 +55,13 @@ exports.new = function (req, res, next) {
 // POST /quizzes/:quizId/tips
 exports.create = function (req, res, next) {
 
+    var authorId = req.session.user && req.session.user.id || 0;
+
     var tip = models.Tip.build(
         {
             text: req.body.text,
-            QuizId: req.quiz.id
+            QuizId: req.quiz.id,
+            AuthorId: authorId
         });
 
     tip.save()
